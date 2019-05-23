c78f2811c69aae76b0376b99deacef788fcb82df
Muhammet Akvardar <34101771+sreilus@users.noreply.github.com>
2019-05-23 05:05:03 +0300

    şifre doğrulama eklendi

---
+++ AracKirala/AracKiralamaApp.WebAPI/AracKiralamaApp.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,21 @@
+﻿using AracKiralamaApp.Business.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AracKiralamaApp.WebAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+		public bool sifreDogrula(string kullaniciAdi, string pass)
+		{
+			using (var kullaniciBusiness = new KullaniciBusiness())
+			{
+				return kullaniciBusiness.KullaniciDogrulama(kullaniciAdi, pass);
+			}
+		}
+	}
+}
